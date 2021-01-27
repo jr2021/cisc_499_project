67546a89698195f776205bc0310d8ae2253fdd21
jr2021 <jake.robertson@queensu.ca>
2021-01-27 10:08:18 -0500

    Added functionality to setup applet

--- notebooks/setup.py
+++ notebooks/setup.py
@@ -4,18 +4,19 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import pandas as pd
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import initialization as init
+import pandas as pd
 
-static_params = {'representation_type': None,
-                 'min': None,
-                 'max': None,
-                 'length': None
+
+static_params = {'permutation' : {'min': None,
+                                  'max': None},
+                 'binary': {},
+                 'integer': {},
+                 'real': {}
                 }
 
-all_options = {'permutation': {'min': None, 
-                               'max': None,
-                              }
-              }
+representation = None
 
 
 def create_app():
@@ -25,23 +26,59 @@ def create_app():
     server = app.server
 
     app.layout = html.Div([
-        dbc.DropdownMenu(id='representation_dropdown',
-                         label="Choose a representation", 
-                         children=[dbc.DropdownMenuItem('Permutation')]),
-        html.Hr(),
-        dbc.Input(id='minimum_value', type='number', min=0, step=1, placeholder='Minimum Value'),
-        html.Hr(),
-        dbc.Input(id='maximum_value', type='number', min=0, step=1, placeholder='Minimum Value')
-        
-        
-    ])    
-    
+        dbc.DropdownMenu(id='rep-dropdown',
+                         label="Choose a representation",
+                         children=[dbc.DropdownMenuItem('Permutation', id='perm-option'),
+                                   dbc.DropdownMenuItem('Binary', id='bin-option'),
+                                   dbc.DropdownMenuItem('Integer', id='int-option'),
+                                   dbc.DropdownMenuItem('Real-Valued', id='real-option')]),
+        dbc.Collapse(id='sel-collapse')
+    ])   
     
     @app.callback(
-        Output('representation_dropdown', 'children'),
-        Input('representation_dropdown', 'children')
+        [Output('sel-collapse', 'is_open'),
+         Output('sel-collapse', 'children'),
+         Output('rep-dropdown', 'label')],
+        [Input('perm-option', 'n_clicks')],
+        [State('sel-collapse', 'is_open')],
     )
-    def update_graph(children):
-        return children
+    def open_perm_sel(n, is_open):
+        if n:
+            representation = 'permutation'
+            return True, [html.Br(),
+                          dbc.Input(id='min-val',
+                                    placeholder='Enter the minimum value for your permutation',
+                                    type='number', 
+                                    min=0, 
+                                    step=1), 
+                          html.Br(),
+                          dbc.Input(id='max-val',
+                                    placeholder='Enter the maximum value for your permutation',
+                                    type='number', 
+                                    min=1, 
+                                    step=1),
+                          html.Br(),
+                          dbc.Button('Create sample population', id='create-perms'),
+                          html.Br(),
+                          dbc.Collapse(id='perms-collapse')], 'Permutation',
+        else:
+            return False, [], 'Choose a representation'
+            
+        
+    @app.callback(
+        [Output('perms-collapse', 'is_open'),
+         Output('perms-collapse', 'children')],
+        [Input('create-perms', 'n_clicks'),
+         Input('min-val', 'value'),
+         Input('max-val', 'value')],
+        [State('perms-collapse', 'is_open')],
+    )
+    def open_perms(n, low, high, is_open):
+        if n:
+            df = pd.DataFrame(init.sample_perm(low, high))
+            return True, [html.Br(), dbc.Table.from_dataframe(df)]
+        else:
+            return False, [] 
+            
     
     return app
