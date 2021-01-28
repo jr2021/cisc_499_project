309c44a61069ad2096f8696a4129f6bbc6853b6f
jr2021 <jake.robertson@queensu.ca>
2021-01-28 18:20:28 -0500

    Improved applet functionality

--- notebooks/.ipynb_checkpoints/setup-checkpoint.py
+++ notebooks/.ipynb_checkpoints/setup-checkpoint.py
@@ -12,6 +12,7 @@ import json
 representation = None
 
 
+
 def create_app():
 
     app = JupyterDash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -19,6 +20,7 @@ def create_app():
     server = app.server
 
     app.layout = html.Div([
+        dcc.Markdown(children='''### Define Static Parameters'''),
         dbc.DropdownMenu(id='rep-dropdown',
                          label="Choose a representation",
                          children=[dbc.DropdownMenuItem('Permutation', id='perm-option'),
@@ -30,59 +32,36 @@ def create_app():
     
     @app.callback(
          Output('sel-collapse', 'is_open'),
-         Output('sel-collapse', 'children'),
          Output('rep-dropdown', 'label'),
+         Output('sel-collapse', 'children'),
          Input('perm-option', 'n_clicks'),
          State('sel-collapse', 'is_open'),
     )
     def open_perm_options(n, is_open):
         if n:
             representation = 'permutation'
-            return True, [html.Br(),
-                          dbc.Input(id='min-val',
-                                    placeholder='Enter the minimum value for your permutation',
-                                    type='number',
-                                    value=0,
-                                    min=0, 
-                                    step=1), 
-                          html.Br(),
-                          dbc.Input(id='max-val',
-                                    placeholder='Enter the maximum value for your permutation',
-                                    type='number', 
-                                    value=1,
-                                    min=1, 
-                                    step=1),
-                          html.Br(),
-                          dbc.Button('Create sample population', id='create-perms'),
-                          html.Br(),
-                          dbc.Collapse(id='perms-collapse')], 'Permutation',
+            return True, 'Permutation', [dbc.Input(id='min-perm-val',
+                                                   placeholder='Enter the minimum value for your permutation',
+                                                   type='number',
+                                                   min=0, 
+                                                   step=1),
+                                         dbc.Input(id='max-perm-val',
+                                                   placeholder='Enter the maximum value for your permutation',
+                                                   type='number',
+                                                   min=1, 
+                                                   step=1),
+                                         dbc.Button('Save static parameters', id='save-perm-params')]
         else:
-            return False, [], 'Choose a representation'
-            
-        
-    @app.callback(
-        Output('perms-collapse', 'is_open'),
-        Output('perms-collapse', 'children'),
-        Input('create-perms', 'n_clicks'),
-        State('min-val', 'value'),
-        State('max-val', 'value'),
-        State('perms-collapse', 'is_open')
-    )
-    def show_perms(n, low, high, is_open):
-        if n:
-            df = pd.DataFrame(init.sample_perm(low, high))
-            return True, [html.Br(), dbc.Table.from_dataframe(df), html.Br(), dbc.Button('Save static parameters', id='save-params')]
-        else:
-            return False, []
+            return False, 'Choose a representation', []
             
     
     @app.callback(
-         Output('save-params', 'children'),
-         Input('save-params', 'n_clicks'),
-         State('min-val', 'value'),
-         State('max-val', 'value')
+         Output('save-perm-params', 'children'),
+         Input('save-perm-params', 'n_clicks'),
+         State('min-perm-val', 'value'),
+         State('max-perm-val', 'value')
     )
-    def save_params(n, low, high):
+    def save_perm_params(n, low, high):
         if n:
             with open('params.txt', 'w') as f:
                 json.dump({'min': low,
