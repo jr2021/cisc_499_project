ab801bb336b4fcceca737f12aa9986b1197b7da4
jr2021 <jake.robertson@queensu.ca>
2021-02-04 13:05:39 -0500

    Configured Configs (sorry) object for testing

--- notebooks/Configuration.py
+++ notebooks/Configuration.py
@@ -3,15 +3,31 @@ import numpy as np
 
 class Config:
 
-    rep = None
-    pop_size = 8
-    par_size = 4
-    off_size = 4
-    gene_size = None
+    
+    pop_size = 32
+    par_size = 16
+    off_size = 16
+    gene_size = 99
     fit_eval = None
-    prob_type = None
+    prob_type, representation, pairing = None, None, None
     num_objs = 1
     pair, select, replace = None, None, None
+    
+    #FIXME
+    def __init__(self):
+        self.representation, self.prob_type, self.pairing = Permutation(self), Single(self), Pairing(self)
+        self.pair, self.select, self.eval, self.replace = self.pairing.adjacent, self.prob_type.rank_based, self.TSP, self.prob_type.rank_based
+    
+    def TSP(pop):
+        dist = np.loadtxt('dist.txt')
+
+        for ind in pop:
+            ind['fitness'][0] += dist[0][ind['gene'][0]]
+            ind['fitness'][0] += np.array([dist[ind['gene'][i - 1]][ind['gene'][i]] for i in range(1, configs.gene_size)]).sum()
+            ind['fitness'][0] += dist[ind['gene'][-1]][0]
+
+        return pop
+
     
 class Pairing:
     
@@ -49,44 +65,39 @@ class Multi:
     def __init__(self, configs):
         self.configs = configs
 
-    def rank_based(self, pop):
-        return np.array(sorted(pop, key=lambda sol: sol['fitness'])[:self.configs.par_size])
+    def NSGA_II(self, pop):
+        pass
     
-
-class Replacement:
-
-    configs = None
-
-    def __init__(self, configs):
-        self.configs = configs
-
-    def rank_based(self, pop):
-        return np.array(sorted(pop, key=lambda sol: sol['fitness'])[:self.configs.pop_size]) 
         
 class Permutation:
 
     configs = None
-    min_value, max_value = None, None
-    mutate, cross, replace = None, None, None
+    min_value, max_value = 1, 99
+    recombination, mutation = None, None
+    recombine, mutate = None, None
+    identity = 'perm'
 
-
+    # FIXME
     def __init__(self, configs):
         self.configs = configs
+        self.recombination, self.mutation = self.Recombination(configs, self), self.Mutation(configs, self)
+        self.recombine, self.mutate = self.recombination.order, self.mutation.swap
 
     def initialize(self):
-        return np.array([{'gene': np.random.permutation(np.arange(start=self.min_value, stop=self.max_value + 1)), 'fitness': np.array([0 for _ in range(self.configs.num_objs)])} for _ in range(self.configs.pop_size)])
+        return np.array([{'gene': np.random.permutation(np.arange(start=self.min_value, stop=self.max_value + 1)), 
+                          'fitness': np.array([0 for _ in range(self.configs.num_objs)])} for _ in range(self.configs.pop_size)])
 
-    class Crossover:
-
-        configs = None
-        perm_configs = None
+    class Recombination:
+        
+        configs, perm_configs = None, None
 
         def __init__(self, configs, perm_configs):
             self.configs, self.perm_configs = configs, perm_configs
 
         def order(self, mother, father):
             x, y = np.random.randint(0, self.configs.gene_size), np.random.randint(0, self.configs.gene_size)
-            off = {'gene': -np.ones(shape=self.configs.gene_size, dtype=np.int), 'fitness': np.array([0 for _ in range(self.configs.num_objs)])}
+            off = {'gene': -np.ones(shape=self.configs.gene_size, dtype=np.int), 
+                   'fitness': np.array([0 for _ in range(self.configs.num_objs)])}
 
             off['gene'][min(x, y):max(x, y)] = mother['gene'][min(x, y):max(x, y)]
 
@@ -118,4 +129,7 @@ class Permutation:
                 off['gene'][i], off['gene'][j] = off['gene'][j], off['gene'][i]
 
             return offs
+        
+        def scramble(self, offs):
+            pass
 
--- notebooks/app.py
+++ notebooks/app.py
@@ -1,56 +1,28 @@
 import json
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from jupyter_dash import JupyterDash
 from Configuration import *
 import pickle
 
-with open('configs.pkl', 'rb') as f:
-    configs = pickle.load(f)
+
+configs = Config()
 
 def create_app():
     
     app = JupyterDash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
     server = app.server
-    app.layout = html.Div([dbc.DropdownMenu(id='objs_dropdown',
-                                            label='Problem type',
-                                            children=[dbc.DropdownMenuItem('single_objective', 
-                                                                           id='sing_option'),
-                                                      dbc.DropdownMenuItem('multi_objective', 
-                                                                           id='multi_option')]),
-                           dbc.Collapse(id='sel_collapse')])
+    app.layout = html.Div([dcc.Dropdown(id='dropdown',
+                                       options=options,
+                                       value=[configs.rep.identity, 'single_obj', 'rank_based', 'order', 'swap', 'tournament'],
+                                       clearable=True,
+                                       multi=True
+                                      ),
+                          dbc.Button('Save dynamic parameters', id='save_dynam')])
     
-    @app.callback(
-        Output('sel_collapse', 'is_open'),
-        Output('objs_dropdown', 'label'),
-        Output('sel_collapse', 'children'),
-        Input('sing_option', 'n_clicks'), prevent_initial_call=True
-    )
-    def sing_obj_sel(n):
-        if n:
-            configs.prob_type = Single(configs)
-            
-            funcs = [f for f in dir(configs.prob_type) 
-                     if callable(getattr(configs.prob_type, f)) and f[0] not in ['_']]
-            
-            children = [dbc.DropdownMenuItem(f, id=f) for f in funcs]
-            sel_dropdown = [dbc.DropdownMenu(id='sel_dropdown',
-                                      label='Parent selection',
-                                      children=children),
-                           dbc.Collapse(id='rec_dropdown')]
-            
-            return True, 'single_objective', sel_dropdown
-        
     
-    @app.callback(
-        Output('sel_dropdown', 'label'),
-        Input('rank_based', 'n_clicks'), prevent_initial_call=True
-    )
-    def rank_based(n):
-        if n:
-            configs.select = configs.prob_type.rank_based
-            
-            return 'rank_based'
+    
     
     return app
